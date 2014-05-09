5663f4bc3ff758f98998b157c440e3f73b6325aa
raghu <easylearnjava4u@gmail.com>
2014-05-09 17:34:06 -0500

    login functionality using Servlets, JSP and MySQL

---
+++ src/com/easylearnjava/dao/LoginDao.java
@@ -0,0 +1,71 @@
+package com.easylearnjava.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class LoginDao {
+	
+	String driver;
+	String url;
+	String userName;
+	String password;
+
+	/**
+	 * Method which fetches password for the user name from the data base
+	 * @param userName
+	 * @return
+	 */
+	public String getUserPassword(String userName){
+
+		Connection conn;
+		PreparedStatement stmt;
+		String passwordFromDB = null;
+
+		try {
+			conn = getDBConnection(); //get the data base connection object
+			String sql = "SELECT user_password FROM  USER where user_name = ? ";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, userName);
+
+			ResultSet res = stmt.executeQuery();
+			while (res.next()) {
+				passwordFromDB = res.getString("USER_PASSWORD");
+			}
+			conn.close(); //close the database connection
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return passwordFromDB;
+	}
+
+	
+	public Connection getDBConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			loadDbProperties();
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, userName, password);
+			//give back the database connection object to the caller
+			return conn;
+		} catch (Exception e) {
+			throw new SQLException(e.getMessage());
+		}
+	}
+
+	/**
+	 * Read the DB configuration parameters from the .properties file using ResourceBundle class
+	 */
+	public void loadDbProperties() {
+		//Resource bundle looks for a file named dbParameters.properties in the class path
+		ResourceBundle labels = ResourceBundle.getBundle("com/easylearnjava/resources/dbParameters");
+		driver = labels.getString("DB_DRIVER");
+		url = labels.getString("DB_URL");
+		userName = labels.getString("DB_USER");
+		password = labels.getString("DB_PASSWORD");
+	}
+	
+}
